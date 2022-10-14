76a403643e33b6976d1693669717b94c167ebf4a
Kefa698 <kefaombongi07@gmail.com>
2022-10-14 15:57:17 +0300

    fixed

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract staking {
-    constructor() {}
+    IERC20 public s_stakingToken;
+
+    constructor(address stakingToken) {
+        s_stakingToken = IERC20(stakingToken);
+    }
 }
